fccc38d0be3cf0256443025a606e5f9405b1ee92
Roberta <robertaf898@gmail.com>
2019-07-12 17:06:13 +0100

    Adds button previous and next in SinglePokemon and navigate to previous and next pokemon

--- src/PokemonComponent.vue
+++ src/PokemonComponent.vue
@@ -58,14 +58,14 @@
         },
 
         created() {
-        console.log('I am the INDEX', this.indexProp);
+        // console.log('I am the INDEX', this.indexProp);
             let linkImages = this.getLinksForImages();
-            console.log("THIS IS THE LINK FOR THE IMAGES IN CREATED", this.linkImages)
+            // console.log("THIS IS THE LINK FOR THE IMAGES IN CREATED", this.linkImages)
         },
 
         watch: {
             linksForImages() {
-                console.log("I am changeddddd:",this.linksForImages);
+                // console.log("I am changeddddd:",this.linksForImages);
                 this.getImagesFromApi() 
             
             } 
@@ -74,7 +74,7 @@
         methods: {
             getLinksForImages() {
                 const axios = require('axios');
-                console.log("THIS IS THE URLLLL", this.indexProp.url)
+                // console.log("THIS IS THE URLLLL", this.indexProp.url)
                 axios.get(this.indexProp.url)
                 .then(response => (this.linksForImages = response.data.forms[0].url));
             
@@ -82,7 +82,7 @@
             
             getImagesFromApi() {
                 const axios = require('axios');
-                    console.log("THIS IS THE LINKKKKKKKK FOR IMAGES", this.linksForImages)
+                    // console.log("THIS IS THE LINKKKKKKKK FOR IMAGES", this.linksForImages)
                     axios.get(this.linksForImages)
                     .then(response => (this.linksForImages = response.data.sprites.front_default))
                     .catch(function (error) {
--- src/SinglePokemon.vue
+++ src/SinglePokemon.vue
@@ -9,8 +9,9 @@
                 </div>
               </v-card-title>
               <v-card-actions>
-                <v-btn flat dark   @click="goToPreviousPokemon">PREVIOUS</v-btn>
-                <v-btn flat dark  @click="goToNextPokemon">NEXT</v-btn>
+                <v-btn flat dark   @click="goToPreviousPokemon"  v-if="previousButton">PREVIOUS</v-btn>
+                <v-btn flat dark  @click="goToNextPokemon" v-if="nextButton">NEXT</v-btn>
+           
               </v-card-actions>
             </v-card>
         </v-flex>
@@ -33,10 +34,17 @@
      
         mounted() {
             this.currentPokemonIndex = 0;
+            //   if (this.currentPokemonIndex = 0) {
+            //            this.currentPokemonIndex++; 
+            //     }
+        
             this.pokemonName = this.infoSinglePokemonProp[this.currentPokemonIndex].name;
-            this.currentPokemonIndex++;
+            console.log("THIS IS THE INDEX IN MOUNTED:", this.currentPokemonIndex);
+            console.log("THIS IS THE POKEMON NAME:", this.pokemonName);
+               console.log("THIS IS THE POKEMON NAME INDEXXX:", this.pokemonName);
+         
             this.infoSinglePokemonProp;
-            console.log(" I AM MOUNTED",this.infoSinglePokemonProp)
+
         },
         
         computed: {
@@ -49,43 +57,56 @@
         data() {
             return {
                 currentPokemonIndex: "",
-                pokemonName: ""
-            
+                pokemonName: "",
+                previousButton: false,
+                nextButton: true
+              
             }
         },
 
         methods: {
+
+            //    countPokemonsNames() {
+            //         console.log("countPokemonsNames", this.infoSinglePokemonProp)
+            //         let totalPokemonsNames = 0;
+            //             if (this.infoSinglePokemonProp == undefined) {
+            //                 return totalPokemonsNames;
+            //             }
+            //             totalPokemonsNames = this.infoSinglePokemonProp.length
+            //             console.log("THIS IS THE TOTAL:", totalPokemonsNames)
+            //             return totalPokemonsNames;
+            //     },
          
             goToNextPokemon() {
-                let pokemonName = this.infoSinglePokemonProp[this.currentPokemonIndex].name;
                 this.currentPokemonIndex++;
+                this.previousButton = true;
+                // let totalPokemonsNames =  this.countPokemonsNames();
+                console.log("THIS IS THE POKEMON NAMEEEEEE BEFORE", this.infoSinglePokemonProp[this.currentPokemonIndex].name)
+                let pokemonName  = this.infoSinglePokemonProp[this.currentPokemonIndex].name;
                 console.log("THIS IS THE CURRENT POKEMON INDEX IN NEXT:", this.currentPokemonIndex)
-
-                console.log("THIS IS MY PROP", pokemonName);
+                console.log("THIS IS THE CURRENT POKEMON NAME IN NEXT:", pokemonName )
                 this.pokemonName = pokemonName;
 
-                if (pokemonName === "raticate") {
-                    this.currentPokemonIndex = 0;
-                    this.currentPokemonIndex++;
-                }
+                 if (this.pokemonName === "raticate") {
+                            this.nextButton = false;
+                        }
+
+                return this.pokemonName
             },
 
-               goToPreviousPokemon() {
-                let pokemonName = this.infoSinglePokemonProp[this.currentPokemonIndex].name;
-                this.currentPokemonIndex--;
+               goToPreviousPokemon(){
+                    this.currentPokemonIndex--; 
+                    let pokemonName = this.infoSinglePokemonProp[this.currentPokemonIndex].name;
+                    this.pokemonName = pokemonName;
+                    console.log("THIS IS THE CURRENT POKEMON INDEX IN  PREVIOUS:", this.currentPokemonIndex)
+                    console.log("THIS IS THE CURRENT POKEMON NAME IN  PREVIOUS:", this.pokemonName);
+                        if (this.pokemonName === "bulbasaur") {
+                            this.previousButton = false;
+                        }
+                    this.nextButton = true;
 
-                console.log("THIS IS MY PROP in previous", pokemonName);
-
-                console.log("THIS IS THE CURRENT POKEMON INDEX IN  PREVIOUS:", this.currentPokemonIndex)
-
-                this.pokemonName = pokemonName;
-
-                 if (pokemonName === "bulbasaur") {
-                    this.currentPokemonIndex = 0;
-                    this.currentPokemonIndex--;
-                }
-            },
-
+                
+                },
 
         }
   
