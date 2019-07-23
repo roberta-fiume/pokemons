efcdc8f917c4c5deb1ea3f2dc96b9518d2f611fb
Roberta <robertaf898@gmail.com>
2019-07-23 18:04:15 +0100

    adds part of videos urls in array

--- src/SinglePokemon.vue
+++ src/SinglePokemon.vue
@@ -9,7 +9,7 @@
                    <span class="title font-weight-bold white--text text--lighten-2 ml-1">{{pokemonName}}</span>
                   </div>
                   <div>
-                        <img :src="singleGif" class="ma-2"/>
+                       <video width="350" controls :src="singleGif"></video>
                   </div>
                 </div>
               </v-card-title>
@@ -34,48 +34,49 @@ import fb from '@/fb'
         watch: {
             infoSinglePokemonProp() {
                 console.log("I am changed:", this.infoSinglePokemonProp)
+            },
+
+            singleGif() {
+                   let linksUrls = [];
+                    let pokemons = ['ivysaur', 'venusaur', 'charmander'];
+                    for (let i = 0; i < pokemons.length; i++) {
+                       let newUrl = this.singleGif.replace("bulbasaur", pokemons[i]);
+                        linksUrls.push(newUrl);
+                    } 
+                 console.log("I AM THE URLS ARRAY:", linksUrls) 
             }
+          
+          
         },
         
      
         mounted() {
+            console.log("this is singleGif in MOUNTED", this.singleGif)
             this.currentPokemonIndex = 0;
             this.pokemonName = this.infoSinglePokemonProp[this.currentPokemonIndex].name;
             console.log("THIS IS THE INDEX IN MOUNTED:", this.currentPokemonIndex);
             console.log("THIS IS THE POKEMON NAME:", this.pokemonName);
             console.log("THIS IS THE POKEMON NAME INDEXXX:", this.pokemonName);
-
             this.infoSinglePokemonProp;
 
-            // this.currentGifIndex = 0;
+            this.currentGifIndex = 0;
             // this.singleGif = this.gifsArray[this.currentGifIndex];
-            // console.log("THIS IS SINGLE GIF", this.singleGif);
-
-          
-
-            
-          var storage = firebase.storage();
-   
-
+            // console.log("THIS IS SINGLE GIF", this.singleGif)
+       
+            var storage = firebase.storage();
             let storageRef = storage.ref();
             let videoRef = storageRef.child('videos/bulbasaur.mp4');
-                   console.log("THIS IS CONTEXT BEFORE THE FUNCTION", this)
-                function getVideosFromStorage() {
-                     videoRef.getDownloadURL().then(function(url)                             {
-                    console.log("THIS IS THE URL", url);
-                      console.log("THIS IS CONTEXT INSIDE THE FUNCTION", this)
-                    let bulbasaur = url;
-                    //   this.singleGif = bulbasaur;
-                    console.log("this is BULBASAUR", bulbasaur);
-                    //  console.log("THIS IS THE SINGLE GIF",this.singleGif);
-                    }).catch(function(error) {
-                        console.error(error);
-                    });
-                }
-            console.log("THIS IS CONTEXT AFTER THE FUNCTION", this)
-            const getVideosFromStorageFunc = getVideosFromStorage.bind(this);
-            getVideosFromStorageFunc();
-           
+               function getVideosFromStorage() {
+                     videoRef.getDownloadURL().then(videoUrl => {
+                        this.singleGif = videoUrl;
+                        console.log("THIS IS THE URL", videoUrl)
+                     })
+                     .catch(err => console.error(err));
+                    console.log("THIS AFTER API CALL", this);
+                };
+                const getVideosFromStorageFunc = getVideosFromStorage.bind(this);
+                getVideosFromStorageFunc();
+            this.replaceStringsInUrl();
         },
         
         computed: {
@@ -91,31 +92,32 @@ import fb from '@/fb'
                 pokemonName: "",
                 previousButton: false,
                 nextButton: true,
-                gifsArray: [
-                'https://media.giphy.com/media/I2nZMy0sI0ySA/giphy.gif', 
-                'https://media.giphy.com/media/GA3BbCXTEm6hq/giphy.gif', 
-                'https://media.giphy.com/media/905ATvUcKqN0I/giphy.gif',
-                'https://media.giphy.com/media/yhfTY8JL1wIAE/giphy.gif',
-                'https://pkmneclipse.net/images/letsgo/kanto-normal/Charmeleon.gif',
-                'https://media.giphy.com/media/48cVVAf9vam9W/giphy.gif', 
-                'https://media.giphy.com/media/fjgqYjVkzfQ9a/giphy.gif', 
-                'https://media.giphy.com/media/jymDyv7MzGgzC/giphy.gif',
-                'https://media.giphy.com/media/tDT5nL8EXbQhW/giphy.gif',
-                'https://media.giphy.com/media/ZKaR6dsqUELIs/giphy.gif',
-                'https://pkmneclipse.net/images/letsgo/kanto-normal/Metapod.gif',
-                'https://media.giphy.com/media/Ktr7TBN3hV39u/giphy.gif',
-                'https://media.giphy.com/media/anLxZPExDZZ4I/giphy.gif',
-                'https://pkmneclipse.net/images/letsgo/kanto-normal/Kakuna.gif', 
-                'https://pkmneclipse.net/images/letsgo/kanto-normal/Beedrill.gif', 
-                'https://pkmneclipse.net/images/letsgo/kanto-normal/Pidgey.gif', 
-                'https://media.giphy.com/media/hJugcAt6VIzMA/giphy.gif', 
-                'https://pkmneclipse.net/images/letsgo/kanto-normal/Pidgeot.gif', 
-                'https://pkmneclipse.net/images/letsgo/kanto-normal/Rattata.gif', 
-                'https://pkmneclipse.net/images/letsgo/kanto-normal/Raticate.gif'
-                ],
+                // gifsArray: [
+                // 'https://media.giphy.com/media/I2nZMy0sI0ySA/giphy.gif', 
+                // 'https://media.giphy.com/media/GA3BbCXTEm6hq/giphy.gif', 
+                // 'https://media.giphy.com/media/905ATvUcKqN0I/giphy.gif',
+                // 'https://media.giphy.com/media/yhfTY8JL1wIAE/giphy.gif',
+                // 'https://pkmneclipse.net/images/letsgo/kanto-normal/Charmeleon.gif',
+                // 'https://media.giphy.com/media/48cVVAf9vam9W/giphy.gif', 
+                // 'https://media.giphy.com/media/fjgqYjVkzfQ9a/giphy.gif', 
+                // 'https://media.giphy.com/media/jymDyv7MzGgzC/giphy.gif',
+                // 'https://media.giphy.com/media/tDT5nL8EXbQhW/giphy.gif',
+                // 'https://media.giphy.com/media/ZKaR6dsqUELIs/giphy.gif',
+                // 'https://pkmneclipse.net/images/letsgo/kanto-normal/Metapod.gif',
+                // 'https://media.giphy.com/media/Ktr7TBN3hV39u/giphy.gif',
+                // 'https://media.giphy.com/media/anLxZPExDZZ4I/giphy.gif',
+                // 'https://pkmneclipse.net/images/letsgo/kanto-normal/Kakuna.gif', 
+                // 'https://pkmneclipse.net/images/letsgo/kanto-normal/Beedrill.gif', 
+                // 'https://pkmneclipse.net/images/letsgo/kanto-normal/Pidgey.gif', 
+                // 'https://media.giphy.com/media/hJugcAt6VIzMA/giphy.gif', 
+                // 'https://pkmneclipse.net/images/letsgo/kanto-normal/Pidgeot.gif', 
+                // 'https://pkmneclipse.net/images/letsgo/kanto-normal/Rattata.gif', 
+                // 'https://pkmneclipse.net/images/letsgo/kanto-normal/Raticate.gif'
+                // ],
                 singleGif: "hello",
-                currentGifIndex: ""
-              
+                currentVideoIndex: "",
+                pokemons: []
+             
             }
         },
 
@@ -165,7 +167,7 @@ import fb from '@/fb'
                 },
 
                 goToNextGifPokemon() {
-                    this.currentGifIndex++;
+                    this. currentVideoIndex++;
                     let singleGif  = this.gifsArray[this.currentGifIndex];
                     console.log("THIS IS THE CURRENT GIF INDEX IN NEXT:", this.currentGifIndex)
                     console.log("THIS IS THE CURRENT POKEMON GIF IN NEXT:", singleGif )
@@ -180,6 +182,12 @@ import fb from '@/fb'
                     console.log("THIS IS THE CURRENT GIF IN  PREVIOUS:", this.singleGif);
                   
                 },
+
+                replaceStringsInUrl() {
+          
+                    console.log("THIS IS BULBASAUR", this.singleGif);
+                }
+           
 
      
         }
